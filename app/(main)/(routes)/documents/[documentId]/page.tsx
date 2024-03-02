8728511610e8d3ed6ee1ed4c4783ad8c45fe9135
shuaibu-shehu <shuiabushehukalifa@gmail.com>
2024-03-02 22:17:43 +0530

    emoji modal added

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,7 +1,42 @@
+'use client'
+
+import { Toolbar } from '@/components/toolbar'
+import { api } from '@/convex/_generated/api'
+import { Id } from '@/convex/_generated/dataModel'
+import { useQuery } from 'convex/react'
 import React from 'react'
 
-export default function DocumentId() {
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">
+  }
+}
+
+
+export default function DocumentId({
+  params
+}: DocumentIdPageProps) {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId
+  })
+
+  if (document === undefined) {
+    return <div>Loading</div>
+  }
+
+  if (document === null) {
+    return (
+      <div>Not Found</div>
+    )
+  }
+
   return (
-    <div>DocumentId</div>
+    <div className='pb-40'>
+      <div className=' md:max-w-3xl lg:max-w-4xl mx-auto'>
+        <div  className='h-[35vh]'/>
+        <Toolbar initialData= {document} />
+    
+      </div>
+    </div>
   )
 }
